9ad26f208c4644df75a1a9634076e092bf9ff101
kfulain <89740818+stadial@users.noreply.github.com>
2022-01-13 21:59:49 +0300

    feat: change axios baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
 	// Axios module configuration: https://go.nuxtjs.dev/config-axios
 	axios: {
 		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-		baseURL: '/',
+		baseURL: 'https://kfu-reg.github.io/database/',
 	},
 
 	// PWA module configuration: https://go.nuxtjs.dev/pwa
