0b60b1323a727f88f51fe8de712c5bf8f8508687
kfulain <89740818+stadial@users.noreply.github.com>
2022-01-14 18:48:12 +0300

    fix: remove `console.log`

--- components/ClassesSelectedStats.vue
+++ components/ClassesSelectedStats.vue
@@ -28,7 +28,6 @@ export default {
   },
   computed: {
     totalCredits() {
-      console.log(this.selectedClasses)
       return this.selectedClasses.reduce((a, b) => a + b.credits, 0)
     },
   },
